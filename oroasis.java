084dd210fa737b2a76c242436c0a9860b70b8147
NAGA MONISH KUMAR I <nagamonishkumar@gmail.com>
2026-03-13 10:31:34 +0530

    hackerearth problem

---
+++ oroasis.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        sc.nextLine();
+        String S=sc.nextLine();
+         
+         System.out.println(N*2);
+         System.out.println(S);
+    }
+}
+//https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/io-exercise/
