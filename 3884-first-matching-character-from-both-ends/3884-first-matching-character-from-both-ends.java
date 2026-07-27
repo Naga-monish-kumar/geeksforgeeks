0f26a761e0eed37f626c0e04c0ee6fd231c8301e
NAGA MONISH KUMAR I <nagamonishkumar@gmail.com>
2026-07-27 14:22:38 +0530

    Time: 1 ms (37.94%), Space: 43.5 MB (31.88%) - LeetHub

--- 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.java
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.java
@@ -1,11 +1,9 @@
 class Solution {
     public int firstMatchingIndex(String s) {
-        int count=0;
         int end=s.length()-1;
         int start=0;
        while(start<=end){
-             if(s.charAt(start)==s.charAt(end)){
-              //  count++;
+             if(s.charAt(start)==s.charAt(end)){           
                 return start;
              }
              start++;
