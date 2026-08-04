0d2bac4853ffe79e704f43a0007ad13c509d31d4
NAGA MONISH KUMAR I <nagamonishkumar@gmail.com>
2026-08-04 13:23:03 +0530

    Time: 7 ms (22.5%), Space: 47.4 MB (11.76%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        ArrayList<Integer> list=new ArrayList<>();
+        Arrays.sort(nums);
+        int j=0;
+        for(int i=nums[0];i<=nums[nums.length-1];i++){
+            if(j<nums.length && i==nums[j] ){
+             
+              j++;
+            }else{
+            list.add(i);
+        }
+        }
+        return list;
+    }
+}
