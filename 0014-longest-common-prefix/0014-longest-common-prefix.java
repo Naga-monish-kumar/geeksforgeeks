83e8a4395c11af8a8588afbfc15071e28b1913c5
NAGA MONISH KUMAR I <nagamonishkumar@gmail.com>
2026-08-05 13:28:46 +0530

    Time: 7 ms (4.87%), Space: 42.2 MB (99.93%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String a=strs[0];
+        String b=strs[strs.length-1];
+        String c="";
+
+        for(int i=0;i<a.length();i++){
+            if(a.charAt(i)==b.charAt(i)){
+              c+=a.charAt(i);
+            }else{
+                break;
+            }
+            
+        }
+        return c;
+    }
+}
