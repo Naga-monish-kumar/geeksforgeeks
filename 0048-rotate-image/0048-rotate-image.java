94e4c9279df97167f10630526991a0a46e91b482
NAGA MONISH KUMAR I <nagamonishkumar@gmail.com>
2026-08-05 13:32:38 +0530

    Time: 0 ms (100%), Space: 43.7 MB (72%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int[][] max = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                max[j][n - 1 - i] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = max[i][j];
+            }
+        }
+    }
+}
